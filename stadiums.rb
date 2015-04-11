58f79d80cd25e56e16f2136f8b203a9cbe6f1d9e
Gerald Bauer <gerald.bauer@gmail.com>
2015-04-11 19:43:38 +0200

    add stadiums

---
+++ stadiums.rb
@@ -0,0 +1,15 @@
+####################################
+# Datafile for Stadiums/Grounds
+#
+#  use
+#    $ sportdb new stadiums
+
+
+world 'openmundi/world.db', setup: 'countries'
+## add world austria.db ??
+## add world deutschland.db ??
+
+
+#####
+# national teams
+football 'openfootball/grounds'   ## NOTE: default is setup: 'all'
