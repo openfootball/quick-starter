59a4a3501f3f17dc04bffccb40f4f5289352ead9
Gerald Bauer <gerald.bauer@gmail.com>
2015-08-25 09:03:10 +0200

    up

---
+++ en2015-16.rb
@@ -0,0 +1,10 @@
+################################################
+# Datafile for English Premier League 2015/16
+#
+#  use
+#    $ sportdb new en2015-16
+
+world  'world.db', setup: 'countries'
+
+football 'eng-england', setup: '2015-16'
+
